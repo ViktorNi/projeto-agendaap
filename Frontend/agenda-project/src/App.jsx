82cc8a39925ec3595fdd23b771b5e0638f874a3d
ViktorNi <95939701+ViktorNi@users.noreply.github.com>
2026-09-16 18:45:51 -0300

    alt1

--- Frontend/agenda-project/src/App.jsx
+++ Frontend/agenda-project/src/App.jsx
@@ -1,64 +1,47 @@
 
+const atendimentos = [
+  {
+    nome: "Maria",
+    horario: "14:00",
+    servico: "Tarot",
+  },
+  {
+    nome: "João",
+    horario: "15:00",
+    servico: "Consulta",
+  },
+  {
+    nome: "Flávio",
+    horario: "16:00",
+    servico: "Tarot",
+  },
+]
+
 function App() {
-  
   return (
-  <div>
-    <header>
-      <p>Minha Agenda</p>
-    </header>
+    <div>
+      <header>
+        <p>Minha Agenda</p>
+      </header>
 
-    <main>
-      <h1>Olá!!</h1>
-      <p>confira seus próximos atendimentos</p>
+      <main>
+        <h1>Olá!!</h1>
+        <p>Confira seus próximos atendimentos</p>
 
+        <h2>Próximos atendimentos</h2>
 
-      <h2>próximos atendimentos</h2>
-
-      <section>
-        <p>Maria: 14 horas - Tarot.</p>
-      </section>
-    </main>
-
-  </div>
+        {atendimentos.map((atendimento) => {
+          return (
+            <section>
+              <p>{atendimento.nome}</p>
+              <p>{atendimento.horario}</p>
+              <p>{atendimento.servico}</p>
+            </section>
+          )
+        })}
+      </main>
+    </div>
   )
 }
 
 export default App
-
-
-const atendimentos = {
-  nome: "maria",
-  horario: "14:00",
-  serviço: "Tarot",
-}
-
-atendimentos.nome = "ana";
-atendimentos.horario = "15:00";
-atendimentos.serviço = "Baralho Cigano"; 
-
-const atendimentos1 = {
-  nome: "joão",
-  horario: "10:00",
-  serviço: "consulta",
-} 
-
-const atendimentos2 = {
-  nome: "flavio",
-  horario: "16:00",
-  serviço: "Tarot",
-} 
-
-const atendimentos3 = {
-  nome: "Jean",
-  horario: "18:00",
-  serviço: "Tarot",
-} 
-
-const atendimentosgeral = [
-  atendimentos, atendimentos1, atendimentos2,
-  atendimentos3
-]
-
-atendimentosgeral.map((atendimento){
-
-})
